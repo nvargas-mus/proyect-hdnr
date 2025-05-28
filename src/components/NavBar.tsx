d140fdaeadc24e66894121a9c530a1f4fafb2972
Nicole Vargas <cvargas.mus@gmail.com>
2025-05-28 12:49:03 -0400

    cambio usuario

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 import Logo from '../assets/logo.png';
+import '../styles/NavbarStyle.css';
+
 
 const Navbar: React.FC = () => {
   const navigate = useNavigate();
@@ -52,10 +54,43 @@ const Navbar: React.FC = () => {
         <a className="navbar-brand" href="#" style={{ marginLeft: '0' }}>
           <img src={Logo} alt="Hidronor" style={{ height: '50px' }} />
         </a>
-        {isLoggedIn && !isAdminPage && (
-          <button className="btn btn-outline-danger" onClick={handleLogout}>
-            Cerrar Sesión
-          </button>
+        {isLoggedIn && (
+        <div className="d-flex align-items-center gap-2">
+          <span className="navbar-user-greeting">
+            Hola, {localStorage.getItem('user_email')?.split('@')[0]}
+          </span>
+
+            {!isAdminPage && (
+              <div className="dropdown">
+                <button
+                  className="dropdown-toggle user-avatar-btn"
+                  type="button"
+                  id="userDropdown"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false"
+                >
+                  <img
+                    src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
+                    alt="Usuario"
+                    className="rounded-circle"
+                  />
+                </button>
+                <ul className="dropdown-menu dropdown-menu-end" aria-labelledby="userDropdown">
+                  <li>
+                    <button className="dropdown-item" onClick={() => navigate('/configuracion')}>
+                      <i className="bi bi-gear me-2"></i> Configuración Usuario
+                    </button>
+                  </li>
+                  <li><hr className="dropdown-divider" /></li>
+                  <li>
+                    <button className="dropdown-item text-danger" onClick={handleLogout}>
+                      <i className="bi bi-box-arrow-right me-2"></i> Cerrar Sesión
+                    </button>
+                  </li>
+                </ul>
+              </div>
+            )}
+          </div>
         )}
       </div>
     </nav>
